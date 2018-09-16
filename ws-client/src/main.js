3434bba1034117475ce9a3c2846de4d0672ded7c
Vinicius Azevedo <viniazvd@gmail.com>
2018-09-15 23:13:54 -0300

    init

--- ws-client/src/main.js
+++ ws-client/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import Root from './Root.vue'
+import Chat from './Chat.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(Root)
+  render: h => h(Chat)
 }).$mount('#app')
